1b12ff005b8a66adf6a9be08b8080beea0f66bb3
DrElucidator <dr.elucidator@gmail.com>
2026-06-27 21:58:34 -0300

    Adicionado IRepositório

---
+++ eAgendaWeb/Modulos/ModuloDespesas/Dominio/IRepositorioDespesa.cs
@@ -0,0 +1,6 @@
+using eAgendaWeb.Compartilhado.Dominio;
+using eAgendaWeb.Modulos.ModuloDespesas.Dominio;
+
+namespace eAgendaWeb.Modulos.ModuloDespesas.Dominio;
+
+public interface IRepositorioDespesa : IRepositorio<Despesa> { }
