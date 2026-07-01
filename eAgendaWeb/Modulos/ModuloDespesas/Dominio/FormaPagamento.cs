ed340a3fb4adb053cd0e25c838a82aaba2a6e749
DrElucidator <dr.elucidator@gmail.com>
2026-07-01 00:25:01 -0300

    Estilo ajustado para visualização global de enum

--- eAgendaWeb/Modulos/ModuloDespesas/Dominio/FormaPagamento.cs
+++ eAgendaWeb/Modulos/ModuloDespesas/Dominio/FormaPagamento.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace eAgendaWeb.Modulos.ModuloDespesas.Dominio;
 
 public enum FormaPagamento
 {
+    [Display(Name = "À Vista")]
     AVista = 1,
+
+    [Display(Name = "Crédito")]
     Credito = 2,
+    
+    [Display(Name = "Débito")]
     Debito = 3
 }
